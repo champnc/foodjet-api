22a7818463954f00f70e1a0668fa0ee16b877212
champnc <natdanai.dev@gmail.com>
2022-05-18 20:37:08 +0700

    fix import prisma module

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,10 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './utils/http-exception.filter';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('NestJS Swagger')
     .setDescription('API description')
@@ -19,6 +20,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
--- nestjs/src/menu/menu.module.ts
+++ nestjs/src/menu/menu.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MenuController],
   providers: [MenuService],
 })
